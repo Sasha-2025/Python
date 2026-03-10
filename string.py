983a3b39ed147d4f4b25c2fc7282c6bfc64039b4
Sasha-2025 <sashasakr8@gmail.com>
2026-03-10 18:04:44 +0200

    Add files via upload

---
+++ string.py
@@ -0,0 +1,14 @@
+first_name = "codingal"
+last_name = "educator"
+
+full_name = first_name + last_name
+print(full_name)
+print()
+
+word ='coding'
+
+print("length of Sting :", len(word))
+print("first letter of String :", word[0])
+print("last letter of String :", word[5])
+
+print("Sliced String :", word[0:3])
