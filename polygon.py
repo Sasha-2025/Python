84cfd3c6172322115e59741283d2fb92b60baf5f
Sasha-2025 <sashasakr8@gmail.com>
2026-03-17 18:16:51 +0200

    Add files via upload

---
+++ polygon.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.Screen().bgcolor("Green") # window to draw
+
+pen = turtle.Turtle() # pen activated
+
+pen.forward(100) # base
+
+pen.left(120)
+pen.forward(100)
+
+pen.left(120)
+pen.forward(100)
+
+turtle.done()
