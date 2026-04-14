70e7fb152cb4b9aaa5f437f1817513dc3658df1a
Sasha-2025 <sashasakr8@gmail.com>
2026-04-14 19:08:38 +0300

    Add files via upload

---
+++ absolute.py
@@ -0,0 +1,12 @@
+def absolute_value(number):
+    
+    if number >= 0:
+        return number
+    else:
+        return -number
+
+
+print("absolute value of 89 :", absolute_value(89))
+
+print("absolute value of -189 :", absolute_value(-189))
+
