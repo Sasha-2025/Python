23c62b389bf10d4345e4982bb76073b9e14e0f9d
Sasha-2025 <sashasakr8@gmail.com>
2026-05-05 19:16:28 +0300

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,25 @@
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+
+my_tuple = (1, "Hello", 3.4, True)
+print(my_tuple)
+
+
+#            0   1   2   3   4   5
+my_tuple = ('p','e','r','m','i','t')
+
+print(my_tuple[5])
+
+
+
+# nested tuple
+
+#                 0        1          2
+nestedtuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+print(nestedtuple[1][2])
+
+
+for letter in my_tuple:
+    print("Hello", letter)
